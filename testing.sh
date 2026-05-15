bc4f142b28c308ffb07217a47cffac779a1b721f
Felix Schneider <mail@fschneider.me>
2026-05-15 23:05:07 +0200

    Add testing script

---
+++ testing.sh
@@ -0,0 +1,25 @@
+#!/opt/homebrew/bin/bash
+
+readonly URL=http://localhost:8000/
+
+source .env
+
+rm bank_app.db
+rm -rf alembic/versions/*
+
+poetry run alembic revision --autogenerate -m "Initial"
+poetry run alembic upgrade head
+poetry run uvicorn source.main:app --reload &
+command_pid=$!
+echo "Command PID: ${command_pid}"
+sleep 1
+echo -e "\n\n"
+
+curl ${URL}users
+
+curl -X POST ${URL}users -H "Content-Type: application/json" -d '{"name": "Felix"}'
+
+echo -e "\n\nKilling process"
+kill $command_pid
+sleep 1
+echo
