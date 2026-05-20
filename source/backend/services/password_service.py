c0386124ed17d7b423d07b58a7d6a914040474ce
Felix Schneider <mail@fschneider.me>
2026-05-20 16:52:41 +0200

    Add some logging

--- source/backend/services/password_service.py
+++ source/backend/services/password_service.py
@@ -15,5 +15,7 @@ def verify_password(password_hash: str, password_to_verify: str) -> bool:
     try:
         _password_hasher.verify(hash=password_hash, password=password_to_verify)
     except VerifyMismatchError:
+        logger.debug("Password verification failed: mismatch")
         return False
+    logger.debug("Password verification succeeded")
     return True
