853087c20656007abba7cb8997d44ed884572979
Felix Schneider <mail@fschneider.me>
2026-05-29 09:46:01 +0200

    Update fill_db_with_testdata.py script

--- source/backend/services/migrations.py
+++ source/backend/services/migrations.py
@@ -5,11 +5,14 @@ from source.backend.logging_utils import get_logger
 
 logger = get_logger(__name__)
 
-_ALEMBIC_INI = get_root_path_of_repository() / "alembic.ini"
+_REPO_ROOT = get_root_path_of_repository()
+_ALEMBIC_INI = _REPO_ROOT / "alembic.ini"
+_ALEMBIC_SCRIPTS = _REPO_ROOT / "source" / "backend" / "alembic"
 
 
 def upgrade_to_head() -> None:
     logger.info(f"Applying database migrations to head using {_ALEMBIC_INI}")
     config = Config(file_=str(_ALEMBIC_INI))
+    config.set_main_option(name="script_location", value=str(_ALEMBIC_SCRIPTS))
     command.upgrade(config=config, revision="head")
     logger.info("Database migrations now are at head")
