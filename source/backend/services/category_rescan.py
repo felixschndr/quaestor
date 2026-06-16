6c5a08130a4f9ccb80674a7a7fdf17cd79feb17c
Felix Schneider <mail@fschneider.me>
2026-06-16 23:23:14 +0200

    Fix logging format for rescanned categories

--- source/backend/services/category_rescan.py
+++ source/backend/services/category_rescan.py
@@ -30,7 +30,7 @@ def _rescan(db_session: Session) -> None:
         checked += 1
         new_category = TransactionCategory.from_transaction(transaction=transaction)
         if new_category != TransactionCategory.UNKNOWN:
-            logger.info(f"Re-scanned {transaction} to {new_category}")
+            logger.info(f"Re-scanned {transaction.to_string_for_transaction_categorization()} to {new_category}")
             transaction.category = new_category
             updated += 1
     db_session.commit()
